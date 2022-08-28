d5622ba1c46c3cfe34bf51dfe27d0c0ace88edfc
Yasin Aysan <yaysan00@gmail.com>
2022-08-28 16:22:51 +0300

    PostService implementing PostController

--- src/main/java/com/yasin/blogapi/controller/PostController.java
+++ src/main/java/com/yasin/blogapi/controller/PostController.java
@@ -5,6 +5,7 @@ import com.yasin.blogapi.entity.Post;
 import com.yasin.blogapi.repository.CategoryRepository;
 import com.yasin.blogapi.repository.PostRepository;
 import com.yasin.blogapi.repository.TagRepository;
+import com.yasin.blogapi.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,46 +17,42 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/post")
 public class PostController {
-    @Autowired
-    private final PostRepository postRepository;
-
-    @Autowired
-    private final TagRepository tagRepository;
-
-    @Autowired
-    private final CategoryRepository categoryRepository;
 
 
-    public PostController(PostRepository postRepository, TagRepository tagRepository, CategoryRepository categoryRepository) {
-        this.postRepository = postRepository;
-        this.tagRepository = tagRepository;
-        this.categoryRepository = categoryRepository;
+
+    private final PostService postService;
+
+    public PostController(PostService postService) {
+        this.postService = postService;
     }
-
 
     @PostMapping
     public ResponseEntity<Post> createPost(@RequestBody Post post){
-        Post post1= postRepository.save(post);
-        return ResponseEntity.status(HttpStatus.CREATED).body(post1);
+        Post createdPost= postService.createPost(post);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdPost);
     }
 
     @GetMapping
     public List<Post> getAllPost(){
-        return postRepository.findAll();
+        return postService.getAllPost();
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Post> getPostById(@PathVariable Long id){
-        return ResponseEntity.ok(postRepository.findById(id).get());
+        return ResponseEntity.ok(postService.getPostById(id));
     }
 
 
     @PostMapping("/{id}/comment")
     public ResponseEntity<Post> createCommentByPostId(@PathVariable Long id, @RequestBody Comment comment){
-        Optional<Post> postOptional = postRepository.findById(id);
-        postOptional.get().getComments().add(comment);
 
-        Post post = postRepository.save(postOptional.get());
-        return ResponseEntity.status(HttpStatus.CREATED).body(post);
+        Post createCommentByPost = postService.createCommentByPostId(id, comment);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(createCommentByPost);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletePostById(@PathVariable Long id){
+        postService.deletePostById(id);
     }
 }
