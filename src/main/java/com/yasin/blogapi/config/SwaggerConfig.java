08221a8631a7fa060f076c9afedb7d3e0a1a8008
Yasin Aysan <yasinaysann@gmail.com>
2022-09-01 19:03:40 +0300

    Swagger2 configuration added

---
+++ src/main/java/com/yasin/blogapi/config/SwaggerConfig.java
@@ -0,0 +1,36 @@
+package com.yasin.blogapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+
+
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig  {
+    @Bean
+    public Docket api(){
+        return new Docket(DocumentationType.SWAGGER_2).groupName("Blog")
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.yasin.blogapi"))
+                .build();
+    }
+
+    private ApiInfo apiInfo(){
+        return new ApiInfoBuilder().title("Blog-API")
+                .description("Platform where people can freely share articles")
+                .contact(new Contact("Yasin Aysan","https://www.linkedin.com/in/yasin-aysan/","yasinaysann@gmail.com"))
+                .license("MIT License")
+                .version("1.0.0").build();
+    }
+
+}
